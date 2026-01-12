bf2653c368abfc4d7bc389923cb9acc5ca0fafc6
Nguyễn Nhất Huy <116622843+not-for-tomorrow@users.noreply.github.com>
2026-01-12 16:20:10 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Chu-Nghia-Xa-Hoi/">
+    <BrowserRouter basename="/chu-nghia-xa-hoi/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
